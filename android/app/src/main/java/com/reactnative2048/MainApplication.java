58d89f2d3ce61a52f2f6fa5b18a6183579d8cb06
Sasan Naderi <sasan.naderi@gmail.com>
2017-10-28 12:31:22 -0700

    Integrate Android SDK. Update react-native.

--- android/app/src/main/java/com/reactnative2048/MainApplication.java
+++ android/app/src/main/java/com/reactnative2048/MainApplication.java
@@ -1,10 +1,15 @@
 package com.reactnative2048;
 
 import android.app.Application;
+import android.util.Log;
+
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.buddybuild.sdk.BuddyBuild;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,8 +18,8 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public boolean getUseDeveloperSupport() {
+      return false;
     }
 
     @Override
@@ -28,5 +33,13 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+
+    BuddyBuild.setup(this);
   }
 }
